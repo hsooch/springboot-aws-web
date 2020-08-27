f69b5769b71d7cec8c8531d6e780517f6927b9fe
razorsc <razor@kakao.com>
2020-08-27 22:31:09 +0900

    저장 테스트

--- src/main/java/com/razoris/bootweb/web/IndexController.java
+++ src/main/java/com/razoris/bootweb/web/IndexController.java
@@ -1,13 +1,21 @@
 package com.razoris.bootweb.web;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     @GetMapping("/")
     public String index() {
         return "index";
+    }
+
+    @GetMapping("/posts/save")
+    public String postsSave() {
+        System.out.println("저장..");
+        return "posts-save";
     }
 }
